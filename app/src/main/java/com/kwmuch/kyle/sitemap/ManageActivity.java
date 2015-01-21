a682a407c06d4673778c137dbf7f8cbe30157bc4
Kyle Muchmore <kmuchmor@gmail.com>
2015-01-20 23:49:17 -0700

    Started on the Manage screen

---
+++ app/src/main/java/com/kwmuch/kyle/sitemap/ManageActivity.java
@@ -0,0 +1,15 @@
+package com.kwmuch.kyle.sitemap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kyle on 1/20/2015.
+ */
+public class ManageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage);
+    }
+}
---
+++ app/src/main/java/items/WorkSite.java
@@ -0,0 +1,63 @@
+package items;
+
+import android.graphics.Bitmap;
+
+import com.google.android.gms.location.Geofence;
+import java.util.Date;
+
+/**
+ * Created by Kyle on 1/20/2015.
+ */
+public class WorkSite {
+
+    private String mSiteName;
+    private Geofence mSiteFence;
+    private Date mLastUpdated;
+    private String mFolderPath;
+    private int mNumPics;
+
+    public WorkSite(String mSiteName, Geofence mSiteFence) {
+        this.mSiteName = mSiteName;
+        this.mSiteFence = mSiteFence;
+    }
+
+    public void addPic(Bitmap image)
+    {
+        /* @TODO Figure out how to save image to card */
+
+        mNumPics++;
+        mLastUpdated = new Date();
+    }
+
+    public String getmSiteName() {
+        return mSiteName;
+    }
+
+    public void setmSiteName(String mSiteName) {
+        this.mSiteName = mSiteName;
+    }
+
+    public Geofence getmSiteFence() {
+        return mSiteFence;
+    }
+
+    public void setmSiteFence(Geofence mSiteFence) {
+        this.mSiteFence = mSiteFence;
+    }
+
+    public Date getmLastUpdated() {
+        return mLastUpdated;
+    }
+
+    public String getmFolderPath() {
+        return mFolderPath;
+    }
+
+    public void setmFolderPath(String mFolderPath) {
+        this.mFolderPath = mFolderPath;
+    }
+
+    public int getmNumPics() {
+        return mNumPics;
+    }
+}
