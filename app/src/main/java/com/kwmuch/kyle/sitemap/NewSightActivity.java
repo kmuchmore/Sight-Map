68c154ea6de57b66fa479915dad6923af76383b2
Kyle Muchmore <kmuchmor@gmail.com>
2015-02-09 19:34:57 -0700

    Made map buttons and fixed getPositions

--- app/src/main/java/com/kwmuch/kyle/sitemap/NewSightActivity.java
+++ app/src/main/java/com/kwmuch/kyle/sitemap/NewSightActivity.java
@@ -3,18 +3,25 @@ package com.kwmuch.kyle.sitemap;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.EditText;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Vector;
+
+import items.Sight;
 
 /**
  * Created by Kyle on 1/26/2015.
@@ -22,13 +29,20 @@ import com.google.android.gms.maps.model.LatLng;
 public class NewSightActivity extends FragmentActivity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        OnMapReadyCallback,
-        LocationListener {
+        LocationListener,
+        GoogleMap.OnMapLongClickListener,
+        OnMapReadyCallback{
 
     private MapFragment mMapFragment = null;
     private GoogleMap mMap = null;
-    private Location lastLocation = null;
+    private Location mCurrentLocation = null;
     public GoogleApiClient mGoogleApiClient = null;
+    private LocationRequest mLocReq = null;
+    private EditText mSightName = null;
+    private Vector<MarkerOptions> fencePoints = new Vector<>();
+    boolean mIsReqLocUpdates = false;
+    Sight mSight = null;
+    //@TODO Pass in Sight and have it return that... not sure how
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,12 +52,17 @@ public class NewSightActivity extends FragmentActivity implements
         mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.gMap);
         mMapFragment.getMapAsync(this);
 
+        mSightName = (EditText) findViewById(R.id.sightName);
+
         buildGoogleApiClient();
+        createLocationRequest();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -57,17 +76,35 @@ public class NewSightActivity extends FragmentActivity implements
 
     @Override
     public void onConnected(Bundle bundle) {
-        lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if(lastLocation != null)
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mCurrentLocation != null)
         {
             CameraPosition update = new CameraPosition.Builder()
-                .target(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()))
-                .zoom( 17.5F )
-                .bearing( 300F) // orientation
-                .tilt( 50F) // viewing angle
+                .target(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))
                 .build();
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(update));
         }
+
+        if(!mIsReqLocUpdates)
+        {
+            startLocationUpdates();
+        }
+
+    }
+
+    protected void createLocationRequest() {
+        mLocReq = new LocationRequest();
+//        mLocReq.setInterval(60000);
+//        mLocReq.setFastestInterval(10000);
+        mLocReq.setInterval(8000);
+        mLocReq.setFastestInterval(5000);
+        mLocReq.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocReq, this);
+        mIsReqLocUpdates = true;
     }
 
     @Override
@@ -77,11 +114,54 @@ public class NewSightActivity extends FragmentActivity implements
 
     @Override
     public void onLocationChanged(Location location) {
-
+        Log.w("Location", "Location Updating");
+        mCurrentLocation = location;
+        updateMapLocation();
+//        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+//        updateUI();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+        mIsReqLocUpdates = false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && !mIsReqLocUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    private void updateMapLocation() {
+        CameraPosition update = new CameraPosition.Builder()
+                .target(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))
+                .zoom(13.9f)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(update));
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+        String sightName = mSightName.getText().toString();
+
+        fencePoints.add(new MarkerOptions()
+            .position(latLng)
+            .title("Fence: " + sightName)
+        );
     }
 }
